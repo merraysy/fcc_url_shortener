3f28288ae773b356803741c2beb9348f8347cd13
Mohammed Erraysy <merraysy@gmail.com>
2016-03-29 17:12:47 +0000

    Add api.js

---
+++ api.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    
+    res.end('Working...');
+    
+});
+
+var port = process.env.PORT;
+
+app.listen(port, function() {
+   
+   console.log('Listening on port ' + port + '...');
+    
+});
